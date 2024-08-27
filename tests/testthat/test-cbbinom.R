8b8891726ae507d3e8893a26ba770569e560e850
zhuxr11 <zxr6@163.com>
2024-08-27 22:20:13 +0800

    Refine tests and establish uniform parameter interface.

--- tests/testthat/test-cbbinom.R
+++ tests/testthat/test-cbbinom.R
@@ -1,16 +1,26 @@
 library(testthat)
 library(extraDistr)
 
+# Parameters
+test_size <- 10
+test_alpha <- 2
+test_beta <- 4
+test_delta <- 1e-6
+
+# Inputs
 test_x <- 5
-test_val <- pcbbinom(q = test_x, size = 10, alpha = 2, beta = 4)
+test_val <- pcbbinom(q = test_x, size = test_size, alpha = test_alpha, beta = test_beta)
 
 testthat::test_that(
   "dcbbinom",
   {
     testthat::expect_equal(
-      dcbbinom(x = test_x, size = 10, alpha = 2, beta = 4),
-      (pcbbinom(q = test_x + 1e-9, size = 10, alpha = 2, beta = 4) - test_val) / 1e-9,
-      tolerance = 1e-6
+      dcbbinom(x = test_x, size = test_size, alpha = test_alpha, beta = test_beta),
+      (pcbbinom(q = test_x + test_delta, size = test_size,
+                alpha = test_alpha, beta = test_beta, log.p = TRUE) -
+         pcbbinom(q = test_x - test_delta, size = test_size,
+                  alpha = test_alpha, beta = test_beta, log.p = TRUE)) /
+        (2 * test_delta) * test_val
     )
   }
 )
@@ -20,7 +30,8 @@ testthat::test_that(
   {
     testthat::expect_equal(
       test_val,
-      extraDistr::pbbinom(q = test_x - 1, size = 10, alpha = 2, beta = 4)
+      extraDistr::pbbinom(q = test_x - 1, size = test_size,
+                          alpha = test_alpha, beta = test_beta)
     )
   }
 )
@@ -29,7 +40,7 @@ testthat::test_that(
   "qcbbinom",
   {
     testthat::expect_equal(
-      qcbbinom(p = test_val, size = 10, alpha = 2, beta = 4),
+      qcbbinom(p = test_val, size = test_size, alpha = test_alpha, beta = test_beta),
       test_x
     )
   }
@@ -39,7 +50,7 @@ testthat::test_that(
   "rcbbinom",
   {
     testthat::expect_error(
-      rcbbinom(n = 10L, size = 10, alpha = 2, beta = 4),
+      rcbbinom(n = 10L, size = test_size, alpha = test_alpha, beta = test_beta),
       NA
     )
   }
@@ -49,8 +60,11 @@ testthat::test_that(
   "gen_hypergeo",
   {
     testthat::expect_equal(
-      gen_hypergeo(U = c(1 - test_x, 10 + 1 - test_x, 10 + 1 - test_x + 4),
-                   L = c(10 + 2 - test_x, 10 + 1 - test_x + 2 + 4),
+      gen_hypergeo(U = c(1 - test_x,
+                         test_size + 1 - test_x,
+                         test_size + 1 - test_x + test_beta),
+                   L = c(test_size + test_alpha - test_x,
+                         test_size + 1 - test_x + test_alpha + test_beta),
                    x = 1,
                    tol = 1e-6,
                    max_iter = 10000L,
